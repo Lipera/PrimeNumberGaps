97011165769ad3fe489eb2233bc2e76d41ba059d
João Pedro Pereira <joao.pedro.pereira@tecnico.ulisboa.pt>
2017-01-17 19:22:24 +0100

    Frist try in generating a plot with many box plots

--- scripts/fileGeneration.r
+++ scripts/fileGeneration.r
@@ -1,6 +1,16 @@
-# Opening files
-prime = read.csv("data/primeNumber.csv")
+#Installing and Loading necessary packages
+install.packages("ggplot2")
+install.packages("reshape2")
+install.packages("Hmisc")
+library(ggplot2)
+library(reshape2)
+library(Hmisc)
 
+# Opening files
+prime = read.csv("data/primeNumber.csv", header = FALSE, sep = " ");
+
+#Not a beautiful manner of removing the first useless column from the files
+#prime$CBO2002 = "prime"
 
 #Saving data to access from the R markdown
 save(prime, file = "data/primeNumber.Rdata")
